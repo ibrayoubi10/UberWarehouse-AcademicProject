bab524fda0c36bc16d2a49e40ec4a405ad8f7a62
ibrayoub10 <ayoubi192003@gmail.com>
2024-11-15 23:25:56 +0100

    prix

--- table.sql
+++ table.sql
@@ -130,6 +130,16 @@ CREATE TABLE Course (
     ID_VEHICULE INT, 
     ID_METEO INT,
     ID_COURSE INT PRIMARY KEY
+    Prix NUMBER,
+    Distance_Parcourue NUMBER(10,2),
+    Duree_Du_Trajet INT,
+    Nombre_Passagers INT,
+    Statut_Course VARCHAR2(20) CHECK(Statut_Course IN('Demande', 'Confirmation', 'Prise_en_Charge', 'Termine')),
+    Temps_dattente_client INT,
+    Code_Promo VARCHAR2(10),
+    Motif_Annulation VARCHAR2(255),
+    Revenue_moyen_par_course NUMBER(10,2),
+    Indice_Satisfaction_Client NUMBER(10,2)
 );
 
     /*
